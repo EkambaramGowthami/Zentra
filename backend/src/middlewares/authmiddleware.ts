64340a2df77bd8fc5a27e147dd81e999874313fe
Ekambaram Gowthami <ekambaramgowthami759@gmail.com>
2025-08-08 11:08:26 +0530

    Update authmiddleware.ts

--- backend/src/middlewares/authmiddleware.ts
+++ backend/src/middlewares/authmiddleware.ts
@@ -1,10 +1,9 @@
-import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 const SECRETE = (process.env.SECRETE || "defaultsecrete") as string;
 
 
 
-export const authmeddleware = async (req:Request,res:Response,next:NextFunction) => {
+export const authmeddleware = async (req,res,next) => {
     const authHeader=req.headers.authorization;
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
         return res.status(401).json({ message: "Token missing or invalid" });
